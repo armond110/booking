4015c1f6e74e00afb9442b9feebc60935ac27af1
Armond Kurbalaj <armondkurbalaj1@gmail.com>
2025-01-10 01:58:20 +0100

    Update CORS options to restrict origin to specific domain for enhanced security

--- api/index.js
+++ api/index.js
@@ -27,12 +27,21 @@ app.use(
     origin: true,
   })
 );
-app.options('*', (req, res) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.send();
-});
+app.options(
+  '*',
+  cors((req, res) => {
+    res.header(
+      'Access-Control-Allow-Origin',
+      'https://booking-green-nine.vercel.app'
+    );
+    res.header(
+      'Access-Control-Allow-Methods',
+      'GET, POST, PUT, DELETE, OPTIONS'
+    );
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.send();
+  })
+);
 
 mongoose.connect(process.env.MONGO_URL);
 
