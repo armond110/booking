ac18b1c72aadc8ae901a17b948fc9675fd190eef
Armond Kurbalaj <armondkurbalaj1@gmail.com>
2025-01-10 01:49:34 +0100

    Restrict CORS origin to specific domain for enhanced security

--- api/index.js
+++ api/index.js
@@ -24,7 +24,7 @@ app.use('/uploads', express.static(__dirname + '/uploads'));
 app.use(
   cors({
     credentials: true,
-    origin: true,
+    origin: 'https://booking-green-nine.vercel.app',
   })
 );
 app.options('*', cors());
