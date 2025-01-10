1e9da057649dfbe6474070ce9804873e379631f0
Armond Kurbalaj <armondkurbalaj1@gmail.com>
2025-01-10 23:55:30 +0100

    Refactor API index to integrate JWT and configure CORS options

--- api/api/index.js
+++ api/api/index.js
@@ -1,12 +1,13 @@
 const app = require('../index.js');
-
-// const cors = require('cors');
-// app.use(
-//   cors({
-//     credentials: true,
-//     origin: true,
-//   })
-// );
-// app.options('*', cors());
-app.listen(3000);
+const jwt = require('jsonwebtoken');
+const cors = require('cors');
+//app.use(
+//  cors({
+//    credentials: true,
+//    origin: true,
+//  })
+//);
+app.options('*', cors());
+//app.listen(3000);
 module.exports = app;
+module.exports = { jwt };
