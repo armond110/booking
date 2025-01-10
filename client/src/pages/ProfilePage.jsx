e198e9c22d8a272aa39f045a941d19d983f730a1
Armond Kurbalaj <armondkurbalaj1@gmail.com>
2025-01-10 01:25:56 +0100

    Refactor vercel.json routes configuration and add CORS options handling in ProfilePage component

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -5,6 +5,8 @@ import axios from "axios";
 import PlacesPage from "./PlacesPage";
 import AccountNav from "../AccountNav";
 
+app.options('*', cors());
+
 
 export default function ProfilePage() {
   const [redirect, setRedirect] = useState(null);
