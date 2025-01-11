973c4fe4504fd4f88b18343d909aa0c9277ece0b
Armond Kurbalaj <armondkurbalaj1@gmail.com>
2025-01-12 00:26:57 +0100

    Update axios base URL for API requests

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,7 +14,7 @@ import BookingsPage from './pages/BookingsPage'
 import BookingPage from './pages/BookingPage'
 import CountryPage from './pages/CountryPage'
 
-axios.defaults.baseURL = 'https://travellingbackend.vercel.app';
+axios.defaults.baseURL = 'https://booking-29xs.onrender.com';
 axios.defaults.withCredentials = true;
 
 function App() {
