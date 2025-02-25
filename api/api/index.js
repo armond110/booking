ee29cc943693f003a65e16287d13e9ac7310785b
Armond Kurbalaj <armondkurbalaj1@gmail.com>
2025-02-25 14:36:19 +0100

    Enable CORS in API and update Vercel configuration for improved routing

--- api/api/index.js
+++ api/api/index.js
@@ -1,12 +1,12 @@
-// const app = require('../index.js');
+const app = require('../index.js');
 
-//const cors = require('cors');
-//app.use(
-//  cors({
-//    credentials: true,
-//    origin: true,
-//  })
-//);
-//app.options('*', cors());
+const cors = require('cors');
+app.use(
+  cors({
+    credentials: true,
+    origin: true,
+  })
+);
+app.options('*', cors());
 //app.listen(3000);
-// module.exports = app;
+module.exports = app;
